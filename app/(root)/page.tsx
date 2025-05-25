f51fbd1bbc26047900eabd3bc5540c0aa85f65b5
dhrumil246 <dhrumil.work964@gmail.com>
2025-05-25 11:10:12 +0530

    feat: add new banking features and components
    - Added MyBanks, PaymentTransfer, and TransactionHistory pages.
    - Implemented AnimatedCounter component for displaying animated financial figures.
    - Created BankCard component to display individual bank account details.
    - Developed DougnutChart component for visualizing bank data using Chart.js.
    - Introduced HeaderBox for displaying titles and user greetings.
    - Built MobileNav for responsive navigation with a sheet component.
    - Added RightSidebar to show user profile and bank information.
    - Created Sidebar for main navigation with user links.
    - Implemented TotalBalanceBox to summarize account information.
    - Added reusable Button component with variants using Radix UI.
    - Developed Sheet component for modal-like functionality.
    - Updated package.json with new dependencies for charts and UI components.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: 'Dhrumil', lastName: 'Amin' , email: 'dhrumil.work964@gmail.com'};
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox 
+           accounts={[]}
+           totalBanks={1}
+           totalCurrentBalance={1250.35}
+          />
+
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50},{ currentBalance: 500.50}]}
+      />
+      </section>
   )
 }
 export default Home 
